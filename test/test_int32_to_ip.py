c6a467cbd66be4c2c23d1bf3762f3ceb55ec9bb8
cherrydan <cherrydan.1978@gmail.com>
2022-12-30 13:27:42 +0300

    Correct error in second exercise

--- test/test_int32_to_ip.py
+++ test/test_int32_to_ip.py
@@ -4,7 +4,7 @@ from my_funcs.int32_to_ip import int32_to_ip
 
 
 @pytest.mark.parametrize("int32, expected_string", [(2149583361, "128.32.10.1"),
-                                                    (3232235777, "192.168.1.2"),
+                                                    (3232235777, "192.168.1.1"),
                                                     (32, "0.0.0.32"),
                                                     (0, "0.0.0.0")])
 def test_int32_to_ip(int32, expected_string):
