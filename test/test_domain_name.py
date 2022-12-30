043b90950c004b3358a3a256695b3a02bd3e1954
cherrydan <cherrydan.1978@gmail.com>
2022-12-30 12:39:03 +0300

    Make test without error)

--- test/test_domain_name.py
+++ test/test_domain_name.py
@@ -4,7 +4,7 @@ from my_funcs import domain_name
 
 
 @pytest.mark.parametrize("url, expected_result", [("http://google.com", "google"),
-                                                  ("http://google.co.jp", "gogle"),
+                                                  ("http://google.co.jp", "google"),
                                                   ("www.xakep.ru", "xakep"),
                                                   ("https://youtube.com", "youtube")])
 def test_domain_name(url, expected_result):
