52cad0c34e3e60668e958e41a98b0aff14ff81c3
cherrydan <cherrydan.1978@gmail.com>
2023-01-10 11:45:28 +0300

    rewrite test_zeros and zeros func with math.factorial function of library

--- test/test_zeros.py
+++ test/test_zeros.py
@@ -3,7 +3,7 @@ import pytest
 from my_funcs.zeros import zeros
 
 
-@pytest.mark.parametrize("n, expected_result", [(0, 0), (6, 1), (30, 7), (12, 2)])
+@pytest.mark.parametrize("n, expected_result", [(0, 0), (6, 1), (30, 7), (12, 2), (1000, 249)])
 def test_zeros(n, expected_result):
     assert zeros(n) == expected_result
     assert zeros(n) == expected_result
